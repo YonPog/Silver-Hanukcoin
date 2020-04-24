72762dcd3764652aa2b7fa3e9933e9151f8b0b22
yon264 <yonatan.pogrebinsky@gmail.com>
2020-04-24 23:31:49 +0300

    Implemented MessageParser, only the node exchange stage (still no blocks).

--- src/MessageParser.java
+++ src/MessageParser.java
@@ -1,14 +1,105 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MessageParser {
 
     private final byte[] stream;
 
+    /**
+     * @param stream Must be a legal representation of a legal message according to the protocol.
+     */
     public MessageParser(byte[] stream){
         this.stream = stream;
     }
 
+    /**
+     * @return The Message that is represented by the byte stream initializer.
+     */
     public Message toMessage(){
-        // TODO, make and call other private utility functions in this class
-        return null;
+        int cmd = this.parseCmd();
+        ArrayList<Node> nodes = this.parseNodes();
+        return new Message(cmd, nodes);
+    }
+
+    /**
+     * @param data At least 1 byte long.
+     * @return Unsigned integer value of data in big endian.
+     */
+    private static int bytesToInt(byte[] data){
+        int n = data.length;
+        int ret = data[n - 1] & 0xFF;
+        for (int i = n - 2; i >= 0; i--){
+            // Masking to cancel sign extension.
+            ret |= (data[i] & 0xFF) << (8 * (n - i - 1));
+        }
+
+        return ret;
+    }
+
+    /**
+     * @return The cmd part of the protocol specification.
+     */
+    private int parseCmd(){
+        byte[] bytes = Arrays.copyOfRange(this.stream, 0, 4);
+        return bytesToInt(bytes);
+    }
+
+    /**
+     * @return The nodes_count part of the protocol specification.
+     */
+    private int parseNodesCount(){
+        byte[] count = Arrays.copyOfRange(this.stream, 8, 12);
+        return bytesToInt(count);
+    }
+
+    /**
+     * @return An ArrayList representing every node in the network according to the message.
+     */
+    private ArrayList<Node> parseNodes(){
+        ArrayList<Node> nodes = new ArrayList<>();
+        int nodeIndex = 12; // after nodes_count in the protocol
+        int nodesToParse = this.parseNodesCount();
+
+        while (nodes.size() < nodesToParse){
+            Node node = this.parseNode(nodeIndex);
+            nodes.add(node);
+
+            // Length of the current node according to protocol
+            nodeIndex += 8 + node.getName().length() + node.getHost().length();
+        }
+
+        return nodes;
+    }
+
+    /**
+     * @param start_index The index which the node starts within this.stream - the byte representation of the message.
+     * @return The Node represented in this.stream at index start_index.
+     */
+    private Node parseNode(int start_index){
+        // Node starts with the length of the name
+        int nameLen = this.stream[start_index];
+        String name = new String(Arrays.copyOfRange(this.stream,
+                start_index + 1, // Name starts after the name length.
+                start_index + 1 + nameLen));
+
+        // After the name length and name there is the host name length.
+        int hostLenIndex = start_index + 1 + nameLen;
+        int host_len = this.stream[hostLenIndex];
+        String host = new String(Arrays.copyOfRange(this.stream,
+                hostLenIndex + 1, // Host name starts after its length.
+                hostLenIndex + 1 + host_len));
+
+        // After the host name length and name there is the port number.
+        int portIndex = hostLenIndex + 1 + host_len;
+        int port = bytesToInt(Arrays.copyOfRange(this.stream, portIndex, portIndex + 2));
+
+        // Port is 2 bytes, after them there are 4 bytes of the timestamp.
+        int lastSeenTs = bytesToInt(Arrays.copyOfRange(this.stream,
+                portIndex + 2, // 2 bytes after the port.
+                portIndex + 6));
+
+
+        return new Node(name, host, port, lastSeenTs);
     }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,63 @@
 public class Test {
     public static void main(String[] args) {
+        testParser();
         System.out.println("no errors... (so far!)");
+    }
+
+    public static void testParser(){
+        System.out.println("Starting to check MessageParser...");
+        /*
+        cmd = 1
+        nodes_count = 2
+            name = test_name
+            host = 1.3.3.7
+            port = 42
+            timestamp = 123
+
+            name = silver
+            host = 5.4.3.2
+            port = 567
+            timestamp = 987
+         */
+        byte[] b1 = {0, 0, 0, 1, (byte) 0xBE, (byte) 0xef, (byte) 0xbe, (byte) 0xef, 0, 0, 0, 2,
+                9, 116, 101, 115, 116, 95, 110, 97, 109, 101, 7, 49, 46, 51, 46, 51, 46, 55, 0, 42, 0, 0, 0, 123,
+                6, 115, 105, 108, 118, 101, 114, 7, 53, 46, 52, 46, 51, 46, 50, 2, 55, 0, 0, 3, (byte) 219};
+
+        MessageParser parser = new MessageParser(b1);
+        Message message = parser.toMessage();
+        if (message.getCmd() != 1){
+            System.out.println("Error in cmd");
+        }
+        if (message.getNodes_count() != 2){
+            System.out.println("Error in nodes_count");
+        }
+
+        if (!message.getNodes().get(0).getName().equals("test_name")){
+            System.out.println("Error in name of node 1");
+        }
+        if (!message.getNodes().get(0).getHost().equals("1.3.3.7")){
+            System.out.println("Error in host of node 1");
+        }
+        if (message.getNodes().get(0).getPort() != 42){
+            System.out.println("Error in port of node 1");
+        }
+        if (message.getNodes().get(0).getLast_seen_ts() != 123){
+            System.out.println("Error in timestamp of node 1");
+        }
+
+        if (!message.getNodes().get(1).getName().equals("silver")){
+            System.out.println("Error in name of node 2");
+        }
+        if (!message.getNodes().get(1).getHost().equals("5.4.3.2")){
+            System.out.println("Error in host of node 2");
+        }
+        if (message.getNodes().get(1).getPort() != 567){
+            System.out.println("Error in port of node 2");
+        }
+        if (message.getNodes().get(1).getLast_seen_ts() != 987){
+            System.out.println("Error in timestamp of node 2");
+        }
+
+
     }
 }
